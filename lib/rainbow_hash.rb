d2b6746d6fcd840a2577ad82187bc2acf898d12c
Jay Wengrow <jaywngrw@gmail.com>
2016-05-23 11:45:55 -0500

    separating display class

--- lib/rainbow_hash.rb
+++ lib/rainbow_hash.rb
@@ -1,53 +1,14 @@
 require "rainbow_hash/version"
+require "rainbow_hash/display"
 
 module RainbowHash
-  class Display
 
-    def initialize(string)
-      @string = string
-      @color_stack = 30
-      @indentation_level = 0
-    end
-
-    def output
-      new_string = ""
-      @string.each_char do |char|
-        if beginning_delimeter?(char)
-          increment_level
-          new_string += "\n" + (" " * @indentation_level)
-        end
-        new_string += colorize(char)
-        if ending_delimeter?(char)
-          decrement_level
-          new_string += "\n" + (" " * @indentation_level)
-        end
-      end
-      puts new_string
-    end
-
-    private
-
-    def beginning_delimeter?(char)
-      char == "[" || char == "{"
-    end
-
-    def ending_delimeter?(char)
-      char == "]" || char == "}"
-    end
-
-    def colorize(char)
-      "\e[#{@color_stack}m#{char}\e[0m"
-    end
-
-    def increment_level
-      @color_stack += 1
-      @indentation_level += 2
-    end
-
-    def decrement_level
-      @color_stack -= 1
-      @indentation_level -= 2
-    end
+  def display(string)
+    puts "ASDAFS"
+    # display = Display.new(string)
+    # display.output
   end
+
+
 
 end
