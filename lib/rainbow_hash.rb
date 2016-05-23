df09ae982fe6ecc563a8b9278618980f2365acdd
Jay Wengrow <jaywngrw@gmail.com>
2016-05-23 11:55:23 -0500

    proper namespacing

--- lib/rainbow_hash.rb
+++ lib/rainbow_hash.rb
@@ -3,12 +3,4 @@ require "rainbow_hash/display"
 
 module RainbowHash
 
-  def display(string)
-    puts "ASDAFS"
-    # display = Display.new(string)
-    # display.output
-  end
-
-
-
 end
--- lib/rainbow_hash/display.rb
+++ lib/rainbow_hash/display.rb
@@ -1,48 +1,56 @@
-class Display
+module RainbowHash
 
-  def initialize(string)
-    @string = string
-    @color_stack = 30
-    @indentation_level = 0
-  end
+  class Display
 
-  def output
-    new_string = ""
-    @string.each_char do |char|
-      if beginning_delimeter?(char)
-        increment_level
-        new_string += "\n" + (" " * @indentation_level)
-      end
-      new_string += colorize(char)
-      if ending_delimeter?(char)
-        decrement_level
-        new_string += "\n" + (" " * @indentation_level)
-      end
+    def self.print(string)
+      self.new(string).output
     end
-    puts new_string
+
+    def initialize(string)
+      @string = string
+      @color_stack = 30
+      @indentation_level = 0
+    end
+
+    def output
+      new_string = ""
+      @string.each_char do |char|
+        if beginning_delimeter?(char)
+          increment_level
+          new_string += "\n" + (" " * @indentation_level)
+        end
+        new_string += colorize(char)
+        if ending_delimeter?(char)
+          decrement_level
+          new_string += "\n" + (" " * @indentation_level)
+        end
+      end
+      puts new_string
+    end
+
+    private
+
+    def beginning_delimeter?(char)
+      char == "[" || char == "{"
+    end
+
+    def ending_delimeter?(char)
+      char == "]" || char == "}"
+    end
+
+    def colorize(char)
+      "\e[#{@color_stack}m#{char}\e[0m"
+    end
+
+    def increment_level
+      @color_stack += 1
+      @indentation_level += 2
+    end
+
+    def decrement_level
+      @color_stack -= 1
+      @indentation_level -= 2
+    end
   end
 
-  private
-
-  def beginning_delimeter?(char)
-    char == "[" || char == "{"
-  end
-
-  def ending_delimeter?(char)
-    char == "]" || char == "}"
-  end
-
-  def colorize(char)
-    "\e[#{@color_stack}m#{char}\e[0m"
-  end
-
-  def increment_level
-    @color_stack += 1
-    @indentation_level += 2
-  end
-
-  def decrement_level
-    @color_stack -= 1
-    @indentation_level -= 2
-  end
 end
